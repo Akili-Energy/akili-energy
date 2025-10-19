d8817049b3d064b8edad6201f3a056e94031e0a8
Teddy BAHA <bahawilliam1229@gmail.com>
2025-10-19 22:51:54 +0100

    Content

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "material-symbols";
-import "./globals.css";
+import "./globals.scss";
 import { ThemeProvider } from "@/components/theme-provider";
 import { LanguageProvider } from "@/components/language-context";
 import { Toaster } from "@/components/ui/toaster";
